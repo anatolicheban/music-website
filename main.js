3080c2a1c144250d5e6b4ee32c56e98d9fc9712b
Anatoli Cheban <107035461+anatolicheban@users.noreply.github.com>
2024-06-25 17:26:50 +0300

    fixes

--- main.js
+++ main.js
@@ -8,12 +8,46 @@ corrects.forEach((item, i) => {
   const content = item.querySelector(".item-content");
   const wrap = item.querySelector(".item-wrap");
   const header = item.querySelector(".item-header");
-  heights[i] = content.clientHeight;
-  header.addEventListener("click", () => {
-    item.classList.toggle("open");
+  const calcHeight = () => {
+    heights[i] = content.clientHeight;
     wrap.style.height =
       (item.classList.contains("open") ? heights[i] : 0) + "px";
+  };
+  window.addEventListener("resize", calcHeight);
+  calcHeight();
+
+  header.addEventListener("click", () => {
+    item.classList.toggle("open");
+    corrects.forEach((el) => {
+      const wrap = el.querySelector(".item-wrap");
+      if (el !== item) {
+        el.classList.remove("open");
+      }
+      wrap.style.height =
+        (el.classList.contains("open") ? heights[i] : 0) + "px";
+    });
   });
 });
 
-console.log(heights);
+const hiddenText = document.getElementById("hidden-text");
+const showButton = document.getElementById("show-hidden");
+const hWrap = hiddenText.querySelector("p");
+
+let hiddenHeight = hWrap.clientHeight;
+
+const calcHeight = () => {
+  hiddenHeight = hWrap.clientHeight;
+  hiddenText.classList.contains("open")
+    ? (hiddenHeight.style.height = hiddenHeight + "px")
+    : "0";
+};
+
+window.addEventListener("resize", calcHeight);
+
+showButton.addEventListener("click", () => {
+  hiddenText.classList.toggle("open");
+  showButton.classList.add("hide");
+  hiddenText.classList.contains("open")
+    ? (hiddenText.style.height = hiddenHeight + "px")
+    : "0";
+});
