135cf8d9c37e2a42248ab128a08db452faa4defe
Anatoli Cheban <107035461+anatolicheban@users.noreply.github.com>
2024-06-25 12:45:40 +0300

    fixes

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 import "./styles/style.scss";
+
+const corrects = document.querySelectorAll(".corrects-items > .item");
+
+const heights = [];
+
+corrects.forEach((item, i) => {
+  const content = item.querySelector(".item-content");
+  const wrap = item.querySelector(".item-wrap");
+  const header = item.querySelector(".item-header");
+  heights[i] = content.clientHeight;
+  header.addEventListener("click", () => {
+    item.classList.toggle("open");
+    wrap.style.height =
+      (item.classList.contains("open") ? heights[i] : 0) + "px";
+  });
+});
+
+console.log(heights);
