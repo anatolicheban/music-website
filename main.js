02dea6736c595a44df94f442de82293abcc41953
Anatoli Cheban <107035461+anatolicheban@users.noreply.github.com>
2024-07-08 16:57:34 +0300

    tablet

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import "./styles/style.scss";
+import "swiper/css";
+import Swiper from "swiper";
 
 const corrects = document.querySelectorAll(".corrects-items > .item");
 
@@ -50,4 +52,44 @@ showButton.addEventListener("click", () => {
   hiddenText.classList.contains("open")
     ? (hiddenText.style.height = hiddenHeight + "px")
     : "0";
+});
+
+const vSwiper = new Swiper(".videos-swiper.swiper", {
+  slidesPerView: 2.4,
+  spaceBetween: 20,
+});
+
+const vPagination = Array.from(
+  document.querySelectorAll(".videos-pagination span"),
+);
+
+vPagination.forEach((el, i) => {
+  el.addEventListener("click", () => {
+    vSwiper.slideTo(i);
+  });
+});
+
+vSwiper.on("slideChange", (swiper) => {
+  vPagination.forEach((el, i) => {
+    el.classList[swiper.activeIndex === i ? "add" : "remove"]("active");
+  });
+});
+
+const menuButton = document.getElementById("menu-btn");
+const aside = document.querySelector(".mobile-aside");
+
+menuButton.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+  menuButton.classList.toggle("active");
+  aside.classList.toggle("open");
+  document.documentElement.style.overflow =
+    document.documentElement.style.overflow === "hidden" ? "initial" : "hidden";
+
+  aside.querySelectorAll("a").forEach((el) => {
+    el.addEventListener("click", () => {
+      menuButton.classList.remove("active");
+      aside.classList.remove("open");
+      document.documentElement.style.overflow = "initial";
+    });
+  });
 });
